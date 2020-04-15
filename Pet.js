309b76f339d8767f577765e4264c8ea17810027f
leitto10 <46364179+leitto10@users.noreply.github.com>
2020-04-15 12:00:03 -0700

    adding a sound method

--- Pet.js
+++ Pet.js
@@ -1,8 +1,17 @@
 class Pet {
-    constructor(animal, age, breed){
-        this.animal = anima;
+    constructor(animal, age, breed, sound){
+        this.animal = animal;
         this.age = age;
         this.breed = breed;
+        this.sound = sound;
+    }
 
+    speak (){
+        console.log(this.sound);
     }
 }
+
+const nuchin = new Pet("dog", 27, "jotito peludo", "whoofff");
+console.log(nuchin);
+
+nuchin.speak();
